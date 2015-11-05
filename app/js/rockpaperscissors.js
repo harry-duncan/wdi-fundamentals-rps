28147e5a4f1ff53fd2d39f168dea0678e08d1d2e
Harry Duncan <harryduncan@Harrys-MacBook-Pro.local>
2015-11-05 15:18:37 +1100

    added playToFive

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -54,12 +54,41 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    /* YOUR CODE HERE */
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    var playerMove = getPlayerMove()
+    var computerMove = getComputerMove()
+    var winner = getWinner(playerMove,computerMove)
+        
+        while (playerWins < 5 || computerWins < 5) {
+            if (winner === "player"){
+                playerWins +=1;
+            }  else if (winner === "computer"){
+                computerWins +=1;
+            }  else if {
+                playerWins = playerWins;
+                computerWins = computerWins;
+                console.log('Player chose ' + playerMove + ' and Computer chose ' + computerMove);
+                console.log('The score total is ' + playerWins + ' V ' + computerWins);
+            }  else if (playerWins=5){
+                console.log("Player wins the game!");
+            } else if (computerWins=5){
+                console.log("Computer wins the game!");
+            }
+        }
     return [playerWins, computerWins];
 }
+
+
+
+
+
+
+
+
+
 
